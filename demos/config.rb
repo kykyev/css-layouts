24fd7f789af120cf2ae7c6179cf83685c3d64009
Me <me@eblan.(none)>
2013-02-28 12:52:36 +0400

    *

--- demos/config.rb
+++ demos/config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require "css-grids"
 require File.join(File.dirname(__FILE__), '..', '/lib/css-layouts')
 
 # Set this to the root of your project when deployed:
